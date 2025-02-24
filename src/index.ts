a5f18399ada967428b4ee9460e20be0cd0350faa
Jagaa1122 <chjagaa@icloud.com>
2025-02-24 13:00:41 +0800

    backend

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
 import express from "express";
-import cors from "cors";
-
-import bodyParser from "body-parser";
-import { userRoute } from "./routes/user.route";
+import { getMovies } from "./database/mongodb";
 
 const app = express();
 const port = 9999;
-app.use(bodyParser.json());
-
-app.use("/user", userRoute);
-// const userRoute = Router();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/movies", async (req, res) => {
+  try {
+    const { year, imdb } = await req.query;
+    const movies = await getMovies(Number(year), Number(imdb));
+    res.status(200).json({ message: "success", movies: movies });
+  } catch (error) {
+    res.status(500).json({ message: "error", error });
+  }
 });
 
 app.listen(9999, () => {
